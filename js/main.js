31c66c0918876b7b3a9365a7b7c4fcb3625f7fd0
DelDuque7 <viniciusdbsduque@gmail.com>
2022-07-23 21:51:17 -0300

    Exercício JS

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function clicou(){
+    document.getElementById("agradecido").innerHTML = "Obrigado por Clicar";
+
+}
+
+function sexo() {
+    document.getElementById("sexo2").innerHTML = "Foda";
+
+}
+
+function redirecionar(){
+    window.open("https://www.youtube.com/watch?v=VqhSdOSXXdA")
+}
+
+
+function trocar(elemento){
+    elemento.innerHTML = "maquina de sexo";
+    
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "n é que funciona...mas na moral, pq n esninar desse gto ?";
+}
+
+function load() {
+    alert("página carregada")
+}
